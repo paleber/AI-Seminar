6c560aafd20e7ee214a43e373dce718bc8a2248b
Patrick Leber <patrick.leber@freenet.de>
2018-03-22 00:29:59 +0100

    convert python to scala

--- src/Network.scala
+++ src/Network.scala
@@ -1,22 +1,32 @@
-
-
 import scala.util.Random
 
-
-class Network(sizes: List[Int]) {
-
-  val numLayers: Int = sizes.size
-  val biases: List[Double] = sizes.drop(1).map(_ => Random.nextGaussian())
-  // val weights =
-
-  val k = Array(Array(1), Array(2), Array(3))
-
-}
-
-
-
-
 case class Matrix(matrix: Array[Array[Double]]) {
+
+  def +(other: Matrix): Matrix = {
+    Matrix(
+      matrix.indices.map(m =>
+        matrix.head.indices.map(n =>
+          matrix(m)(n) + other.matrix(m)(n)
+        ).toArray
+      ).toArray
+    )
+  }
+
+  def *(other: Matrix): Matrix = {
+    Matrix(
+      matrix.indices.map(m =>
+        other.matrix.head.indices.map(n =>
+          other.matrix.indices.map(i =>
+            matrix(m)(i) * other.matrix(i)(n)
+          ).sum
+        ).toArray
+      ).toArray
+    )
+  }
+
+  def map(f: Double => Double): Matrix = {
+    Matrix(matrix.map(_.map(f)))
+  }
 
   override def toString: String = matrix.map(row =>
     s"(${row.mkString(",")})"
@@ -25,34 +35,8 @@ case class Matrix(matrix: Array[Array[Double]]) {
 }
 
 
-object Start extends App {
-  val a = Matrix(Array.ofDim[Double](2, 3))
-  println(a)
-}
-
 /*
-
-"""
-network.py
-~~~~~~~~~~
-
-A module to implement the stochastic gradient descent learning
-algorithm for a feedforward neural network.  Gradients are calculated
-using backpropagation.  Note that I have focused on making the code
-simple, easily readable, and easily modifiable.  It is not optimized,
-and omits many desirable features.
-"""
-
-#### Libraries
-# Standard library
-import random
-
-# Third-party libraries
-import numpy as np
-
-class Network(object):
-
-    def __init__(self, sizes):
+   def __init__(self, sizes):
         """The list ``sizes`` contains the number of neurons in the
         respective layers of the network.  For example, if the list
         was [2, 3, 1] then it would be a three-layer network, with the
@@ -68,55 +52,102 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+ */
+class Network(sizes: List[Int]) {
 
+  val numLayers: Int = sizes.size
+
+  val biases: List[Matrix] = sizes.drop(1).map(y =>
+    Matrix(Array.fill(y, 1)(Random.nextGaussian))
+  )
+
+  val weights = sizes.dropRight(1).zip(sizes.drop(1)).map(t =>
+    Matrix(Array.fill(t._2, t._1)(Random.nextGaussian))
+  )
+
+  /*
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
+   */
+  def feedForward(matrix: Matrix): Matrix = {
+    var a = matrix
+    biases.zip(weights).foreach(t => // TODO use reduce
+      a = (t._2 * a + t._1).map(sigmoid)
+    )
+    a
+  }
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
-        """Train the neural network using mini-batch stochastic
-        gradient descent.  The ``training_data`` is a list of tuples
-        ``(x, y)`` representing the training inputs and the desired
-        outputs.  The other non-optional parameters are
-        self-explanatory.  If ``test_data`` is provided then the
-        network will be evaluated against the test data after each
-        epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially."""
-        if test_data: n_test = len(test_data)
-        n = len(training_data)
-        for j in xrange(epochs):
-            random.shuffle(training_data)
-            mini_batches = [
-                training_data[k:k+mini_batch_size]
-                for k in xrange(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print "Epoch {0}: {1} / {2}".format(
-                    j, self.evaluate(test_data), n_test)
-            else:
-                print "Epoch {0} complete".format(j)
+  /*
+  def SGD(self, training_data, epochs, mini_batch_size, eta,
+           test_data=None):
+       """Train the neural network using mini-batch stochastic
+       gradient descent.  The ``training_data`` is a list of tuples
+       ``(x, y)`` representing the training inputs and the desired
+       outputs.  The other non-optional parameters are
+       self-explanatory.  If ``test_data`` is provided then the
+       network will be evaluated against the test data after each
+       epoch, and partial progress printed out.  This is useful for
+       tracking progress, but slows things down substantially."""
+       if test_data: n_test = len(test_data)
+       n = len(training_data)
+       for j in xrange(epochs):
+           random.shuffle(training_data)
+           mini_batches = [
+               training_data[k:k+mini_batch_size]
+               for k in xrange(0, n, mini_batch_size)]
+           for mini_batch in mini_batches:
+               self.update_mini_batch(mini_batch, eta)
+           if test_data:
+               print "Epoch {0}: {1} / {2}".format(
+                   j, self.evaluate(test_data), n_test)
+           else:
+               print "Epoch {0} complete".format(j)
+  */
+  def SGD(trainingData: List[(Matrix, Int)], epochs: Int, miniBatchSize: Int, eta: Any, testData: Option[Any]): Unit = {
+    val n = trainingData.size
+    (0 until epochs).foreach { j =>
+      val shuffled = Random.shuffle(trainingData)
+      val mini_batches = (0 to n by miniBatchSize).map(k =>
+        trainingData.slice(k, k + miniBatchSize)
+      )
+      // val updatedMiniBatches =
+      // TODO
+    }
 
-    def update_mini_batch(self, mini_batch, eta):
-        """Update the network's weights and biases by applying
-        gradient descent using backpropagation to a single mini batch.
-        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-        is the learning rate."""
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
+  }
 
-    def backprop(self, x, y):
+
+  /*
+ def update_mini_batch(self, mini_batch, eta):
+      """Update the network's weights and biases by applying
+      gradient descent using backpropagation to a single mini batch.
+      The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+      is the learning rate."""
+      nabla_b = [np.zeros(b.shape) for b in self.biases]
+      nabla_w = [np.zeros(w.shape) for w in self.weights]
+      for x, y in mini_batch:
+          delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+          nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+          nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+      self.weights = [w-(eta/len(mini_batch))*nw
+                      for w, nw in zip(self.weights, nabla_w)]
+      self.biases = [b-(eta/len(mini_batch))*nb
+                     for b, nb in zip(self.biases, nabla_b)]
+ */
+  def updateMiniBatch(miniBatch: List[(Double, Double)], eta: Any): Unit = {
+    val nablaB = biases.map(_.map(_ => 0))
+    val nablaW = weights.map(_.map(_ => 0))
+    for (elem <- miniBatch) {
+      // TODO
+    }
+  }
+
+
+  /*
+   def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
@@ -150,8 +181,10 @@ class Network(object):
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
+   */
 
-    def evaluate(self, test_data):
+  /*
+     def evaluate(self, test_data):
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
         network's output is assumed to be the index of whichever
@@ -159,19 +192,52 @@ class Network(object):
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
+*/
+  def evaluate(testData: List[(Matrix, Int)]) = {
+    testData.map { case (x, y) =>
+      val r = feedForward(x).matrix.head
+      (r.indexOf(r.max), y)
+    }.count {
+      case (x, y) => x == y
+    }
+  }
 
+  /*
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
         return (output_activations-y)
+   */
+  def cost_derivative(output_activations: Double, y: Double): Double = output_activations - y
 
-#### Miscellaneous functions
+}
+
+
+/*
 def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
 
+ */
+def sigmoid(z: Double) = 1 / (1 + math.exp(-z))
+
+/*
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
-
  */
+def sigmoidPrime(z: Double) = sigmoid(z) * (1 - sigmoid(z))
+
+
+object Start extends App {
+
+  val a = Matrix(Array(Array(1.0, 2.0, 3.0), Array(2.0, 3, 4)))
+  println(a)
+  println()
+  val b = Matrix(Array(Array(3.0, 1.0), Array(2.0, 2), Array(7.0, 5)))
+  println(b)
+  println()
+  println(a * b)
+
+}
+
